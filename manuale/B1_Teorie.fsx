d1dbeee69d1d4042b2f7fb980188257f17c48d77
Domenico Masini <domenico.masini@gmail.com>
2020-03-07 17:37:08 +0100

    pulizia

--- manuale/B1_Teorie.fsx
+++ manuale/B1_Teorie.fsx
@@ -1,6 +1,7 @@
 (**
 
 TEORIE
+=============
 
 TOERIA CORE
 ------------
@@ -48,40 +49,42 @@ fsi.AddPrinter print_qterm
 fsi.AddPrinter print_thm
 (***unhide***)
 
-true_def                                                  // istanza della proprieta' riflessiva di uguaglianza per la funzione
-// |- true <=> (\(p:bool). p) = (\p. p)                   // d'identita' booleana
+true_def                                                  // istanza della proprieta' riflessiva di uguaglianza per la 
+// |- true <=> (\(p:bool). p) = (\p. p)                   // funzione d'identita' booleana
 
-conj_def                                                  // funzione binaria che restituisce se il fatto che i due argomenti
-// |- $/\ = (\p1 p2. !p. (p1 ==> (p2 ==> p)) ==> p)       // inisieme implichino il valore, implica il valore
+conj_def                                                  // funzione binaria che restituisce se il fatto che i due 
+// |- $/\ = (\p1 p2. !p. (p1 ==> (p2 ==> p)) ==> p)       // argomenti inisieme implichino il valore, implica il valore
 
-forall_def                                                // funzione che restituisce se il predicato restituisce true per ogni
-// |- $! = (\(P:'a->bool). P = (\x. true))                // input
+forall_def                                                // funzione che restituisce se il predicato restituisce true per 
+// |- $! = (\(P:'a->bool). P = (\x. true))                // ogni input
 
-exists_def                                                // funzione che restituisce per un P se un qualsiasi elemento selezionato
-// |- $? = (\(P:'a->bool). P ($@ P))                      // come soddisfacente il predicato necessariamente soddisfa il predicato
+exists_def                                                // funzione che restituisce per un P se un qualsiasi elemento 
+// |- $? = (\(P:'a->bool). P ($@ P))                      // selezionato come soddisfacente il predicato necessariamente  
+                                                          // soddisfa il predicato
 
-one_one_def                                               // funzione che restituisce se la funzione argomento e' iniettiva, cioe'
-// |- ONE_ONE =                                           // se l'uguaglianza dei valori per due argomenti implica ncessariamente 
-//     (\(f:'a->'b). !x1 x2. f x1 = f x2 ==> x1 = x2)     // l'uguaglianza dei due argomenti
+one_one_def                                               // funzione che restituisce se la funzione argomento e'  
+// |- ONE_ONE =                                           // iniettiva, cioe' se l'uguaglianza dei valori per due argomenti   
+//     (\(f:'a->'b). !x1 x2. f x1 = f x2 ==> x1 = x2)     // implica ncessariamente l'uguaglianza dei due argomenti
 
-type_definition_def                                       // funzione che prende un predicato per elementi del tipo di rappresenta-
-// |- TYPE_DEFINITION =                                   // zione e un mapping da elementi del nuovo tipo al tipo di rappresenta-
-//     (\P (rep:'b->'a). ONE_ONE rep                      // zione e restituisce se il mapping e' iniettivo e mappa su elementi che 
-//                     /\ (!x. P x <=> (?y. x = rep y)))  // soddisfano il predicato. E' usata per definire nuove costanti di tipo
+type_definition_def                                       // funzione che prende un predicato per elementi del tipo di 
+// |- TYPE_DEFINITION =                                   // rappresentazione e un mapping da elementi del nuovo tipo al 
+//     (\P (rep:'b->'a). ONE_ONE rep                      // tipo di  rappresentazione e restituisce se il mapping e' 
+//                     /\ (!x. P x <=> (?y. x = rep y)))  // iniettivo e mappa su  elementi che  soddisfano il predicato. 
+                                                          // E' usata per definire nuove costanti di tipo
 
 (**
 **Assiomi**
 *)
 
-eta_ax                                                     // per ogni funzione f la lambda astrazione dell'applicazione di f a 
-// |- !(f:'a->'b). (\x. f x) = f                           // alla variabile lambda e' uguale alla funzione stessa
+eta_ax                                                     // per ogni funzione f la lambda astrazione dell'applicazione
+// |- !(f:'a->'b). (\x. f x) = f                           // di f a alla variabile lambda e' uguale alla funzione stessa
    
 imp_antisym_ax                                             // proprieta' antisimmetrica dell'implicazione
-// |- !p1 p2. (p1 ==> p2) ==>                              // 
-//              ((p2 ==> p1) ==> (p1 <=> p2))              // 
+// |- !p1 p2. (p1 ==> p2) ==>                               
+//              ((p2 ==> p1) ==> (p1 <=> p2))               
    
-select_ax                                                  // per ogni P e x, se x soddisfa P, allora P e' soddisfatto anche 
-// |- !(P:'a->bool) x. P x ==> P ($@ P)                    // dall'elemento selezionato per P
+select_ax                                                  // per ogni P e x, se x soddisfa P, allora P e' soddisfatto  
+// |- !(P:'a->bool) x. P x ==> P ($@ P)                    // anche dall'elemento selezionato per P
 
 (**
 Logica Predicativa
@@ -98,197 +101,199 @@ Logica Predicativa
 // ONTO             `:('a->'b)->bool`               Nonfix
 // COND             `:bool->'a->'a->'a`             Nonfix *
 
-(**
-DEFINITIONS
+(*** hide ***)
 
-false_def
-   |- false <=> (!p. p)
+//(**
+//DEFINITIONS
 
-not_def
-   |- $~ = (\p. p ==> false)
+//false_def
+//   |- false <=> (!p. p)
 
-disj_def
-   |- $\/ = (\p1 p2. !p. (p1 ==> p) ==> (p2 ==> p) ==> p)
+//not_def
+//   |- $~ = (\p. p ==> false)
 
-uexists_def
-   |- $?! = (\(P:'a->bool). ?x. P x /\ (!y. P y ==> y = x))
+//disj_def
+//   |- $\/ = (\p1 p2. !p. (p1 ==> p) ==> (p2 ==> p) ==> p)
 
-let_def
-   |- LET = (\(f:'a->'b) x. f x)
+//uexists_def
+//   |- $?! = (\(P:'a->bool). ?x. P x /\ (!y. P y ==> y = x))
 
-onto_def
-   |- ONTO = (\(f:'a->'b). !y. ?x. y = f x)
+//let_def
+//   |- LET = (\(f:'a->'b) x. f x)
 
-cond_def
-   |- COND =
-        (\p (t1:'a) t2.
-            @x. ((p <=> true) ==> x = t1) /\ ((p <=> false) ==> x = t2))
+//onto_def
+//   |- ONTO = (\(f:'a->'b). !y. ?x. y = f x)
 
-********************************************************************************
+//cond_def
+//   |- COND =
+//        (\p (t1:'a) t2.
+//            @x. ((p <=> true) ==> x = t1) /\ ((p <=> false) ==> x = t2))
 
-                                  PAIRS
+//********************************************************************************
 
-TYPE CONSTANTS
+//                                  PAIRS
 
-#                `:'1#'2`                        Infix (10, RightAssoc)
+//TYPE CONSTANTS
 
-CONSTANTS
+//#                `:'1#'2`                        Infix (10, RightAssoc)
 
-MkPairRep        `:'a->'b->'a->'b->bool`         Nonfix
-IsPairRep        `:('a->'b->bool)->bool`         Nonfix
-PairAbs          `:('a->'b->bool)->'a#'b`        Nonfix
-PairRep          `:'a#'b->'a->'b->bool`          Nonfix
-PAIR             `:'a->'b->'a#'b`                Nonfix *
-FST              `:'a#'b->'a`                    Nonfix
-SND              `:'a#'b->'b`                    Nonfix
+//CONSTANTS
 
-TYPE DEFINITIONS
+//MkPairRep        `:'a->'b->'a->'b->bool`         Nonfix
+//IsPairRep        `:('a->'b->bool)->bool`         Nonfix
+//PairAbs          `:('a->'b->bool)->'a#'b`        Nonfix
+//PairRep          `:'a#'b->'a->'b->bool`          Nonfix
+//PAIR             `:'a->'b->'a#'b`                Nonfix *
+//FST              `:'a#'b->'a`                    Nonfix
+//SND              `:'a#'b->'b`                    Nonfix
 
-prod_def
-   |- ?(f:'a#'b->'a->'b->bool). TYPE_DEFINITION IsPairRep f
+//TYPE DEFINITIONS
 
-DEFINITIONS
+//prod_def
+//   |- ?(f:'a#'b->'a->'b->bool). TYPE_DEFINITION IsPairRep f
 
-mk_pair_rep_def
-   |- MkPairRep = (\(x:'a) (y:'b) a b. a = x /\ b = y)
+//DEFINITIONS
 
-is_pair_rep_def
-   |- IsPairRep = (\(r:'a->'b->bool). ?a b. r = MkPairRep a b)
+//mk_pair_rep_def
+//   |- MkPairRep = (\(x:'a) (y:'b) a b. a = x /\ b = y)
 
-prod_def
-   |- ?(f:'a#'b->'a->'b->bool). TYPE_DEFINITION IsPairRep f
+//is_pair_rep_def
+//   |- IsPairRep = (\(r:'a->'b->bool). ?a b. r = MkPairRep a b)
 
-prod_bij_def1
-   |- !(a:'a#'b). PairAbs (PairRep a) = a
+//prod_def
+//   |- ?(f:'a#'b->'a->'b->bool). TYPE_DEFINITION IsPairRep f
 
-prod_bij_def2
-   |- !(r:'a->'b->bool). IsPairRep r <=> PairRep (PairAbs r) = r
+//prod_bij_def1
+//   |- !(a:'a#'b). PairAbs (PairRep a) = a
 
-pair_def
-   |- PAIR = (\(x:'a) (y:'b). PairAbs (MkPairRep x y))
+//prod_bij_def2
+//   |- !(r:'a->'b->bool). IsPairRep r <=> PairRep (PairAbs r) = r
 
-fst_def
-   |- FST = (\(p:'a#'b). @x. ?y. p = (x,y))
+//pair_def
+//   |- PAIR = (\(x:'a) (y:'b). PairAbs (MkPairRep x y))
 
-snd_def
-   |- SND = (\(p:'a#'b). @y. ?x. p = (x,y))
+//fst_def
+//   |- FST = (\(p:'a#'b). @x. ?y. p = (x,y))
 
+//snd_def
+//   |- SND = (\(p:'a#'b). @y. ?x. p = (x,y))
 
-********************************************************************************
 
-                                INDIVIDUALS
+//********************************************************************************
 
-TYPE CONSTANTS
+//                                INDIVIDUALS
 
-ind              `:ind`                          Nonfix
+//TYPE CONSTANTS
 
-CONSTANTS
+//ind              `:ind`                          Nonfix
 
-IND_ZERO         `:ind`                          Nonfix
-IND_SUC          `:ind->ind`                     Nonfix
+//CONSTANTS
 
-DEFINITIONS
+//IND_ZERO         `:ind`                          Nonfix
+//IND_SUC          `:ind->ind`                     Nonfix
 
-ind_suc_zero_spec
-   |- ONE_ONE IND_SUC /\ (!i. ~ (IND_SUC i = IND_ZERO))
+//DEFINITIONS
 
-AXIOMS
+//ind_suc_zero_spec
+//   |- ONE_ONE IND_SUC /\ (!i. ~ (IND_SUC i = IND_ZERO))
 
-infinity_ax
-   |- ?(f:ind->ind). ONE_ONE f /\ ~ ONTO f
+//AXIOMS
 
-********************************************************************************
+//infinity_ax
+//   |- ?(f:ind->ind). ONE_ONE f /\ ~ ONTO f
 
-                              NATURAL NUMBERS
+//********************************************************************************
 
-TYPE CONSTANTS
+//                              NATURAL NUMBERS
 
-nat              `:nat`                          Nonfix
+//TYPE CONSTANTS
 
-CONSTANTS
+//nat              `:nat`                          Nonfix
 
-IsNatRep         `:ind->bool`                    Nonfix
-NatAbs           `:ind->nat`                     Nonfix
-NatRep           `:nat->ind`                     Nonfix
-ZERO             `:nat`                          Nonfix
-SUC              `:nat->nat`                     Nonfix
-PRE              `:nat->nat`                     Nonfix
-+                `:nat->nat->nat`                Infix (50, LeftAssoc)
--                `:nat->nat->nat`                Infix (50, LeftAssoc)
-*                `:nat->nat->nat`                Infix (55, LeftAssoc)
-EXP              `:nat->nat->nat`                Infix (60, LeftAssoc)
-EVEN             `:nat->bool`                    Nonfix
-ODD              `:nat->bool`                    Nonfix
-<                `:nat->nat->bool`               Infix (40, NonAssoc)
-<=               `:nat->nat->bool`               Infix (40, NonAssoc)
->                `:nat->nat->bool`               Infix (40, NonAssoc)
->=               `:nat->nat->bool`               Infix (40, NonAssoc)
-BIT0             `:nat->nat`                     Nonfix
-BIT1             `:nat->nat`                     Nonfix
-NUMERAL          `:nat->nat`                     Nonfix
+//CONSTANTS
 
-TYPE DEFINITIONS
+//IsNatRep         `:ind->bool`                    Nonfix
+//NatAbs           `:ind->nat`                     Nonfix
+//NatRep           `:nat->ind`                     Nonfix
+//ZERO             `:nat`                          Nonfix
+//SUC              `:nat->nat`                     Nonfix
+//PRE              `:nat->nat`                     Nonfix
+//+                `:nat->nat->nat`                Infix (50, LeftAssoc)
+//-                `:nat->nat->nat`                Infix (50, LeftAssoc)
+//*                `:nat->nat->nat`                Infix (55, LeftAssoc)
+//EXP              `:nat->nat->nat`                Infix (60, LeftAssoc)
+//EVEN             `:nat->bool`                    Nonfix
+//ODD              `:nat->bool`                    Nonfix
+//<                `:nat->nat->bool`               Infix (40, NonAssoc)
+//<=               `:nat->nat->bool`               Infix (40, NonAssoc)
+//>                `:nat->nat->bool`               Infix (40, NonAssoc)
+//>=               `:nat->nat->bool`               Infix (40, NonAssoc)
+//BIT0             `:nat->nat`                     Nonfix
+//BIT1             `:nat->nat`                     Nonfix
+//NUMERAL          `:nat->nat`                     Nonfix
 
-nat_def
-   |- ?(f:nat->ind). TYPE_DEFINITION IsNatRep f
+//TYPE DEFINITIONS
 
-DEFINITIONS
+//nat_def
+//   |- ?(f:nat->ind). TYPE_DEFINITION IsNatRep f
 
-is_nat_rep_def
-   |- !i. IsNatRep i <=> (!P. P IND_ZERO /\ (!j. P j ==> P (IND_SUC j)) ==> P i)
+//DEFINITIONS
 
-nat_bij_def1
-   |- !a. NatAbs (NatRep a) = a
+//is_nat_rep_def
+//   |- !i. IsNatRep i <=> (!P. P IND_ZERO /\ (!j. P j ==> P (IND_SUC j)) ==> P i)
 
-nat_bij_def2
-   |- !r. IsNatRep r <=> NatRep (NatAbs r) = r
+//nat_bij_def1
+//   |- !a. NatAbs (NatRep a) = a
 
-zero_def
-   |- ZERO = NatAbs IND_ZERO
+//nat_bij_def2
+//   |- !r. IsNatRep r <=> NatRep (NatAbs r) = r
 
-suc_def
-   |- !n. SUC n = NatAbs (IND_SUC (NatRep n))
+//zero_def
+//   |- ZERO = NatAbs IND_ZERO
 
-pre_def
-   |- PRE 0 = 0 /\ (!n. PRE (SUC n) = n)
+//suc_def
+//   |- !n. SUC n = NatAbs (IND_SUC (NatRep n))
 
-add_def
-   |- (!n. 0 + n = n) /\ (!m n. SUC m + n = SUC (m + n))
+//pre_def
+//   |- PRE 0 = 0 /\ (!n. PRE (SUC n) = n)
 
-sub_def
-   |- (!n. n - 0 = n) /\ (!m n. m - SUC n = PRE (m - n))
+//add_def
+//   |- (!n. 0 + n = n) /\ (!m n. SUC m + n = SUC (m + n))
 
-mult_def
-   |- (!n. 0 * n = 0) /\ (!m n. SUC m * n = n + m * n)
+//sub_def
+//   |- (!n. n - 0 = n) /\ (!m n. m - SUC n = PRE (m - n))
 
-exp_def
-   |- (!n. n EXP 0 = 1) /\ (!m n. m EXP SUC n = m * m EXP n)
+//mult_def
+//   |- (!n. 0 * n = 0) /\ (!m n. SUC m * n = n + m * n)
 
-even_def
-   |- (EVEN 0 <=> true) /\ (!n. EVEN (SUC n) <=> ~ EVEN n)
+//exp_def
+//   |- (!n. n EXP 0 = 1) /\ (!m n. m EXP SUC n = m * m EXP n)
 
-odd_def
-   |- !n. ODD n <=> ~ EVEN n
+//even_def
+//   |- (EVEN 0 <=> true) /\ (!n. EVEN (SUC n) <=> ~ EVEN n)
 
-lt_def
-   |- (!m. m < 0 <=> false) /\ (!m n. m < SUC n <=> m = n \/ m < n)
+//odd_def
+//   |- !n. ODD n <=> ~ EVEN n
 
-le_def
-   |- !m n. m <= n <=> m < n \/ m = n
+//lt_def
+//   |- (!m. m < 0 <=> false) /\ (!m n. m < SUC n <=> m = n \/ m < n)
 
-gt_def
-   |- !m n. m > n <=> n < m
+//le_def
+//   |- !m n. m <= n <=> m < n \/ m = n
 
-ge_def
-   |- !m n. m >= n <=> n <= m
+//gt_def
+//   |- !m n. m > n <=> n < m
 
-bit0_def
-   |- (BIT0 ZERO = ZERO) /\ (!n. BIT0 (SUC n) = SUC (SUC (BIT0 n)))
+//ge_def
+//   |- !m n. m >= n <=> n <= m
 
-bit1_def
-   |- !n. BIT1 n = SUC (BIT0 n)
+//bit0_def
+//   |- (BIT0 ZERO = ZERO) /\ (!n. BIT0 (SUC n) = SUC (SUC (BIT0 n)))
 
-numeral_def
-   |- !n. NUMERAL n = n
+//bit1_def
+//   |- !n. BIT1 n = SUC (BIT0 n)
 
-*)
+//numeral_def
+//   |- !n. NUMERAL n = n
+
+//*)
--- manuale/index.fsx
+++ manuale/index.fsx
@@ -2,8 +2,8 @@
 Manuale
 ==================
 
-[1 - introduzione](1_Introduzione.html)
+[Appendice A5 - Regole d'inferenza](A5_Inference_Rules.html)
 
-[Appendice 5 - Regole d'inferenza](A5_Inference_Rules.html)
+[Appendice B1 - Teorie](B1_Teorie.html)
 
 *)
