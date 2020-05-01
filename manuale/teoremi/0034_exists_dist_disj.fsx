67dfc75aeeb307197184ff034aab468fde619a8e
Domenico Masini <domenico.masini@gmail.com>
2020-05-01 17:49:08 +0200

    varie bk

--- manuale/teoremi/0034_exists_dist_disj.fsx
+++ manuale/teoremi/0034_exists_dist_disj.fsx
@@ -12,21 +12,74 @@ Bool.load
 
 exists_dist_disj_thm
 
-([],"!P a. (?(x:a). x = a /\ P x) <=> P a")
+([],"!P Q. (?(x:a). P x \/ Q x) <=> (?x. P x) \/ (?x. Q x)")
 |> start_proof
-|> gen_rule_bk
-|> gen_rule_bk
+|> list_gen_rule_bk
 |> deduct_antisym_rule_bk [] []
-
-|> exists_rule_bk "a:a"
-|> conj_rule_bk [] [0]
-|> refl_conv_bk
+|> disj_cases_rule_bk [0] [] [] "?(x:a). P x" "?(x:a). Q x"
 |> assume_rule_bk
-|> focus_goal
-|> choose_rule_bk [0] [] 0 ("a:a","x:a")
-|> exists_rule_bk "a:a"
-|> conj_rule_bk [] [0]
-|> refl_conv_bk
+|> choose_rule_bk [0] [] 0 ("x:a","x:a")
+|> assume_rule_bk
+|> exists_rule_bk "x:a"
+|> disj1_rule_bk
+|> assume_rule_bk
+|> choose_rule_bk [0] [] 0 ("x:a","x:a")
+|> assume_rule_bk
+|> exists_rule_bk "x:a"
+|> disj2_rule_bk
+|> assume_rule_bk
+|> choose_rule_bk [0] [] 0 ("x:a","x:a")
+|> assume_rule_bk
+//|> focus_goal
+|> disj_cases_rule_bk [0] [] [] "(P:a->bool) x" "(Q:a->bool) x"
+|> assume_rule_bk
+|> disj1_rule_bk
+|> exists_rule_bk "x:a"
+|> assume_rule_bk
+|> disj2_rule_bk
+|> exists_rule_bk "x:a"
+|> assume_rule_bk
+|> view
+|> root
+|> linearizeProof
 
+let px = (parse_term(@"(P:'a->bool) x")) 
+let qx = (parse_term(@"(Q:'a->bool) x")) 
+let x = (parse_term(@"x:'a"))  in
+let tm1 = (parse_term(@"?(x:'a). P x")) 
+let tm2 = (parse_term(@"?(x:'a). Q x")) in
+list_gen_rule [(parse_term(@"P:'a->bool"));(parse_term(@"Q:'a->bool"))]
+
+(disj_cases_rule_fd (assume_rule_fd (parse_term(@"(?(x:'a). P x) \/ (?(x:'a). Q x)")))
+  (* ?x. P x |- ?x. P x \/ Q x                     *)
+  (choose_rule_fd x (assume_rule_fd tm1)
+    (exists_rule_fd (parse_term(@"?(x:'a). P x \/ Q x")) x
+      (disj1_rule_fd (assume_rule_fd px) qx) ))
+  (* ?x. Q x |- ?x. P x \/ Q x                     *)
+  (choose_rule_fd x (assume_rule_fd tm2)
+    (exists_rule_fd (parse_term(@"?(x:'a). P x \/ Q x")) x
+      (disj2_rule_fd px (assume_rule_fd qx)) )))
+|> zipper
 |> view
 
+(deduct_antisym_rule_fd
+  (* (?x. P x) \/ (?x. Q x) |- ?x. P x \/ Q x      *)
+  (disj_cases_rule_fd (assume_rule_fd (parse_term(@"(?(x:'a). P x) \/ (?(x:'a). Q x)")))
+    (* ?x. P x |- ?x. P x \/ Q x                     *)
+    (choose_rule_fd x (assume_rule_fd tm1)
+      (exists_rule_fd (parse_term(@"?(x:'a). P x \/ Q x")) x
+        (disj1_rule_fd (assume_rule_fd px) qx) ))
+    (* ?x. Q x |- ?x. P x \/ Q x                     *)
+    (choose_rule_fd x (assume_rule_fd tm2)
+      (exists_rule_fd (parse_term(@"?(x:'a). P x \/ Q x")) x
+        (disj2_rule_fd px (assume_rule_fd qx)) )))
+  (* ?x. P x \/ Q x |- (?x. P x) \/ (?x. Q x)      *)
+  (choose_rule_fd x (assume_rule_fd (parse_term(@"?(x:'a). P x \/ Q x")))
+    (* P x \/ Q x |- (?x. P x) \/ (?x. Q x)          *)
+    (disj_cases_rule_fd (assume_rule_fd (parse_term(@"P (x:'a) \/ Q x")))
+      (* P x |- (?x. P x) \/ (?x. Q x)                 *)
+      (disj1_rule_fd (exists_rule_fd tm1 x (assume_rule_fd px)) tm2)
+      (* Q x |- (?x. P x) \/ (?x. Q x)                 *)
+      (disj2_rule_fd tm1 (exists_rule_fd tm2 x  (assume_rule_fd qx))) )))
+|> zipper
+|> view
--- manuale/teoremi/0035_exists_one_point.fsx
+++ manuale/teoremi/0035_exists_one_point.fsx
@@ -12,34 +12,80 @@ Bool.load
 
 exists_one_point_thm
 
-([],"!P Q. (?(x:a). P x \/ Q x) <=> (?x. P x) \/ (?x. Q x)")
+([],"!P a. (?(x:a). x = a /\ P x) <=> P a")
 |> start_proof
 |> list_gen_rule_bk
 |> deduct_antisym_rule_bk [] []
-|> disj_cases_rule_bk [0] [] [] "?(x:a). P x" "?(x:a). Q x"
+|> exists_rule_bk "a:a"
+|> conj_rule_bk [] [0]
+|> refl_conv_bk
 |> assume_rule_bk
 |> choose_rule_bk [0] [] 0 ("x:a","x:a")
 |> assume_rule_bk
-|> exists_rule_bk "x:a"
-|> disj1_rule_bk
+|> eq_mp_rule_bk [0] [0] "(P:a->bool) x"
+|> mk_comb2_rule_bk
+|> conjunct1_rule_bk "(P:a->bool) x"
 |> assume_rule_bk
-|> choose_rule_bk [0] [] 0 ("x:a","x:a")
+|> conjunct2_rule_bk "(x:a) = a"
 |> assume_rule_bk
-|> exists_rule_bk "x:a"
-|> disj2_rule_bk
-|> assume_rule_bk
-|> choose_rule_bk [0] [] 0 ("x:a","x:a")
+
+|> view
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+([],"!P a. (?(x:a). x = a /\ P x) <=> P a")
+|> start_proof
+|> gen_rule_bk
+|> gen_rule_bk
+|> deduct_antisym_rule_bk [] []
+|> exists_rule_bk "a:a"
+|> conj_rule_bk [] [0]
+|> refl_conv_bk
 |> assume_rule_bk
 //|> focus_goal
-|> disj_cases_rule_bk [0] [] [] "(P:a->bool) x" "(Q:a->bool) x"
+|> choose_rule_bk [0] [] 0 ("x:a","x:a")
 |> assume_rule_bk
-|> disj1_rule_bk
-|> exists_rule_bk "x:a"
+|> eq_mp_rule_bk [0] [0] "(P:a->bool) x"
+|> mk_comb2_rule_bk
+|> conjunct1_rule_bk "(P:a->bool) x"
 |> assume_rule_bk
-|> disj2_rule_bk
-|> exists_rule_bk "x:a"
+|> conjunct2_rule_bk "(x:a) = a"
 |> assume_rule_bk
+
 |> view
-|> root
-|> linearizeProof
+
+let tm = (parse_term(@"?(x:'a). x = a /\ P x"))
+let s1 = assume_rule_fd (parse_term(@"(x:'a)=a /\ P x"))
+
+(choose_rule_fd (parse_term(@"x:'a")) (assume_rule_fd tm)
+    (eq_mp_rule_fd (mk_comb2_rule_fd (parse_term(@"P:'a->bool")) (conjunct1_rule_fd s1))
+           (conjunct2_rule_fd s1)) )
+|> zipper
+|> view
+
+gen_rule_fd (parse_term(@"P:'a->bool"))
+ (gen_rule_fd (parse_term(@"a:'a"))
+   (deduct_antisym_rule_fd
+     (exists_rule_fd tm (parse_term(@"a:'a"))
+       (conj_rule_fd (refl_conv_fd (parse_term(@"a:'a"))) (assume_rule_fd (parse_term(@"(P:'a->bool) a")))))
+     (choose_rule_fd (parse_term(@"x:'a")) (assume_rule_fd tm)
+       (eq_mp_rule_fd (mk_comb2_rule_fd (parse_term(@"P:'a->bool")) (conjunct1_rule_fd s1))
+              (conjunct2_rule_fd s1)) )))
+|> zipper
+|> view
 
